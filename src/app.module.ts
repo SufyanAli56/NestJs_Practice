3b4e1b66fa0b78af3bc2706dc49c774643a0b584
SufyanAli56 <sufyanalii1127@gmail.com>
2025-10-04 02:40:25 -0700

    exception concept

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,11 @@ import { StudentModule } from './student/student.module';
 import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
 import { UserRolesController } from './user-roles/user-roles.controller';
+import { ExceptionController } from './exception/exception.controller';
 
 @Module({
   imports: [EmployeeModule, CategroiesModule, StudentModule, CustomerModule],
-  controllers: [AppController, UserController, ProductsController, MynameController, UserRolesController],
+  controllers: [AppController, UserController, ProductsController, MynameController, UserRolesController, ExceptionController],
   providers: [AppService,  ProductsService],
 })
 export class AppModule {}
