51617149b9f797a5d79348d877e92d457e1cb148
SufyanAli56 <sufyanalii1127@gmail.com>
2025-10-05 05:28:17 -0700

    middleware done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
@@ -11,10 +11,15 @@ import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
 import { UserRolesController } from './user-roles/user-roles.controller';
 import { ExceptionController } from './exception/exception.controller';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [EmployeeModule, CategroiesModule, StudentModule, CustomerModule],
   controllers: [AppController, UserController, ProductsController, MynameController, UserRolesController, ExceptionController],
   providers: [AppService,  ProductsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+     consumer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
